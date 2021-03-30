ed603b7833ebf2d3e928006358dfd07857da3fd2
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-30 20:54:46 +0300

    Add url creation example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,14 @@
 'use strict';
 
-const user = {
-  firstName: 'Test',
-  lastName: 'Testovich',
-  age:18,
-  isMale:true,
-  sayHello:function(){
-    //Выводит в alert фамилию и имя пользователя.
-    alert(`Hello!!! My name is  ${this.firstName} ${this.lastName}`)
-  },
- isAdult: function(){
-   // >= 18 - true, < 18 -false
-   return this.age >= 18;
-  
- },
+/* Создать функцию, которая возвращает валидный url */
+
+const PROTOCOL = 'http://';
+const PROTOCOL_SAFE = 'https://';
+const SERVER_IP = 'localhost';
+const PORT = 3000;
+
+function createQuery(absPath = '/home/news/index.html', isSafe = true){
+ return`${isSafe ? PROTOCOL_SAFE : PROTOCOL}${SERVER_IP}:${PORT} ${absPath}`;
+
 }
-user.sayHello();
-user.isAdult();
-
-const User = function(firstName, lastName, age, isMale){
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.isMale = isMale;
-  this.sayHello = function(){
-    alert(`My name is  ${this.firstName} ${this.lastName} 
-    Me is ${age}`)
-  };
-  this.isAdult = function(){
-    return this.age >= 18;
-  };
-}
-
-const user1 = new User("John", "Doe", 19, true);
-const user2 = new User("Elizabeth", "Smith", 17, false);
-
+console.log(createQuery());
