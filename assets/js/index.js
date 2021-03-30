e2e992d967f0ef2b2d36bd97e9588d2e40b273d4
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-30 20:16:44 +0300

    Add user constructor example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,37 @@
 'use strict';
 
-const a = +prompt('num1');
-const b = +prompt('num2');
-/*
-alert(a + ' + ' + b + ' = ' + (a + b));
-*/
+const user = {
+  firstName: 'Test',
+  lastName: 'Testovich',
+  age:18,
+  isMale:true,
+  sayHello:function(){
+    //Выводит в alert фамилию и имя пользователя.
+    alert(`Hello!!! My name is  ${this.firstName} ${this.lastName}`)
+  },
+ isAdult: function(){
+   // >= 18 - true, < 18 -false
+   return this.age >= 18;
+  
+ },
+}
+user.sayHello();
+user.isAdult();
 
-alert(`${a} + ${b} = ${a + b}`);
+const User = function(firstName, lastName, age, isMale){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
+  this.sayHello = function(){
+    alert(`My name is  ${this.firstName} ${this.lastName} 
+    Me is ${age}`)
+  };
+  this.isAdult = function(){
+    return this.age >= 18;
+  };
+}
 
-console.log("tst \" stdggts\" fggfgf\n 'sdscsxds' ");
-console.log('sdsdsdsc "sdsssy" df\'gdgfg\n sdsfs');
-
-console.log(`ts't "test" ttt\`af lmf
-kxkkx
-hdh
-xs kkd`);
+const user1 = new User("John", "Doe", 19, true);
+const user2 = new User("Elizabeth", "Smith", 17, false);
 
