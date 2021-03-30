a4c56e69f9db62c15b064dfdea4ce8e384c5db28
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-30 19:37:09 +0300

    Add template string example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,12 @@
 'use strict';
-/*
+
 const a = +prompt('num1');
 const b = +prompt('num2');
-
+/*
 alert(a + ' + ' + b + ' = ' + (a + b));
 */
+
+alert(`${a} + ${b} = ${a + b}`);
 
 console.log("tst \" stdggts\" fggfgf\n 'sdscsxds' ");
 console.log('sdsdsdsc "sdsssy" df\'gdgfg\n sdsfs');
@@ -13,3 +15,4 @@ console.log(`ts't "test" ttt\`af lmf
 kxkkx
 hdh
 xs kkd`);
+
