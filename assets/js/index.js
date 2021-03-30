cab79e1e5d8df28041dc735fd6b3c0d165e85578
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-30 19:26:34 +0300

    Add quotes example

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+/*
+const a = +prompt('num1');
+const b = +prompt('num2');
+
+alert(a + ' + ' + b + ' = ' + (a + b));
+*/
+
+console.log("tst \" stdggts\" fggfgf\n 'sdscsxds' ");
+console.log('sdsdsdsc "sdsssy" df\'gdgfg\n sdsfs');
+
+console.log(`ts't "test" ttt\`af lmf
+kxkkx
+hdh
+xs kkd`);
